886a9e8927f425e79c460e79239e1131e57c5813
afranke00 <alexfranke00@gmail.com>
2023-05-18 19:28:22 +0100

    Updated the code here, will update main.py later

--- request.py
+++ request.py
@@ -1,20 +1,21 @@
 import os
 import json
-from flask import Flask, Response, request, render_template, session
+import requests
 
 # Here are the sentence vectors which will be passed to the function
 data = {'sentence': "This is a trial"}
 
 # The sentence vectors included in json format
-data_json = json.dumps(data)
+json_data = json.dumps(data)
 # The headers are required for the post request
 headers = {"Content-Type": "application/json"}
 # The address of the local server we are posting to
-local_server = 'http://localhost8080/predict'
+local_server = 'http://localhost:8080/predict'
 
 # Defining a function to post the data to the local server and receive a response
-def send_request(json_input):
-    response = request.post(local_server, data=json_input, headers=headers)
-    output = json.loads(response.text)
+def send_request(json_data):
+    response = requests.post(local_server, data=json_data, headers=headers)
+    output = response.text
     return output
 
+print(send_request(json_data))
