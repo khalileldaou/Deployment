4153068301ae389ddd145a95421bd7b25bffeeed
Khalil El Daou <daoukhalil@hotmail.com>
2023-05-18 15:59:52 +0100

    Updated models, format, and fixed time issue

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ app = Flask(__name__)
 vectorizer = load('./savedmodel/tfidf_vectorizer.joblib')
 model = load('./savedmodel/xgboost_model.joblib')
 
+str_label = {
+    0: 'neutral',
+    1: 'confusion/curiosity',
+    2: 'realization/surprise',
+    3: 'sadness/grief',
+    4: 'approval/pride',
+    5: 'admiration',
+    6: 'fear/nervousness/embarrassment',
+    7: 'gratitude/relief',
+    8: 'joy/amusement',
+    9: 'remorse/disappointment',
+    10: 'desire/excitement/optimism',
+    11: 'annoyance/anger',
+    12: 'disapproval/disgust',
+    13: 'love/caring'
+}
 
 @app.route('/')
 def home():
@@ -18,40 +34,37 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predictor():
     if request.method == 'POST':
+        # User enter text
         user_text_input = request.form['text']
+
+        # preprocess text: tokenize, stemm, remove emojis, and stopwords
         processed_text = text_preprocessing_pipeline(user_text_input)
         processed_text_str = ' '.join(processed_text)
+
+        # vectorizer text
         vectorized_text = vectorizer.transform([processed_text_str])
+
+        # predict text using model
         text_prediction = model.predict(vectorized_text)
         text_prediction = int(text_prediction)
 
-        str_label = {
-            0: 'neutral',
-            1: 'confusion/curiosity',
-            2: 'realization/surprise',
-            3: 'sadness/grief',
-            4: 'approval/pride',
-            5: 'admiration',
-            6: 'fear/nervousness/embarrassment',
-            7: 'gratitude/relief',
-            8: 'joy/amusement',
-            9: 'remorse/disappointment',
-            10: 'desire/excitement/optimism',
-            11: 'annoyance/anger',
-            12: 'disapproval/disgust',
-            13: 'love/caring'
-        }
+        # map label to its name
         pred_text = str_label[text_prediction]
 
+        # calculate probabilty of predicted label
         probs = model.predict_proba(vectorized_text)[0]
+
+        # calculate probabilty of all other top labels
         top_probs = probs.argsort()[-10:][::-1]
         top_labels = [str_label[label] for label in top_probs]
         top_percents = [probs[label] for label in top_probs]
         top_preds = list(zip(top_labels, top_percents))
 
+        date_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
         monitor_file = {'input': user_text_input,
                         'prediction': str_label[text_prediction],
-                        'date-time': datetime
+                        'date-time': date_time
                         }
         updateCSV(monitor_file, 'monitor.csv')
 
