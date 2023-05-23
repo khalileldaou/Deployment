e0cd9c099409cab36ed6d594001cbc5a71b82f20
Khalil El Daou <daoukhalil@hotmail.com>
2023-05-23 22:01:41 +0100

    Locust testing added

---
+++ load_testing.py
@@ -0,0 +1,18 @@
+import random
+from locust import HttpUser, task, between
+import json
+
+
+class WebUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def deployment_page(self):
+        self.client.get(url="/")
+    @task
+    def deployment_page(self):
+        random_texts = ["Lorem ipsum dolor sit amet.", "Consectetur adipiscing elit.",
+                        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]
+        user_text = random.choice(random_texts)
+        # json_input = json.dumps({'sentence': random.choice(user_text)})
+        self.client.post(url='/predict', data={'text': user_text})
