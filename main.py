a41904ce54b74afd42ab2eafc1bd1176f44559c8
Khalil El Daou <daoukhalil@hotmail.com>
2023-05-23 17:15:42 +0100

    Cleand main.py and sorted table

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, request
 from joblib import load
 import json
-import numpy as np
 from savedmodels.demo import text_preprocessing_pipeline
 from datetime import datetime
-import csv
-import os
+import pymongo
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# Load vectorzier and xgboost saved models to use for predicting labels for texts
 vectorizer = load('./savedmodels/tfidf_vectorizer.joblib')
 print(vectorizer)
 model = load('./savedmodels/xgboost_model.joblib')
 print(model)
 
+# Map labels numbers to their names to show when text is predicted
 str_label = {
     0: 'neutral',
     1: 'confusion/curiosity',
@@ -32,55 +32,48 @@ str_label = {
     13: 'love/caring'
 }
 
+# Link to MongoDB to store the montitoring data
 MONGODB_URI = 'mongodb+srv://humzamalikramzan:W06dI5K1TnjIb9Jf@nlp.cvp3aqy.mongodb.net/'
 DB_NAME = 'deployment_DB'
 COLLECTION_NAME = 'monitor'
 
+# Read data from MongoDB to display the monitoring data in the web page 
 def read_data_from_mongodb():
     client = MongoClient(MONGODB_URI)
     db = client.get_database(DB_NAME)
     collection = db[COLLECTION_NAME]
     
-    data = collection.find()
+    data = collection.find().sort("date_time", pymongo.DESCENDING)
     result = []
     for document in data:
         result.append(document)
-    
+    print(result)
     client.close()
     return result
-
-# def updateCSV(dict_object, file_name):
-#     # Get the absolute path of the file
-#     script_dir = os.path.dirname(os.path.abspath(__file__))
-#     file_path = os.path.join(script_dir, file_name)
-
-#     # Write the dictionary to the CSV file
-#     columns = ['input', 'prediction', 'date-time']
-#     with open(file_path, 'a', newline='') as file:
-#         writer = csv.DictWriter(file, fieldnames=columns)
-#         writer.writerow(dict_object)
-        
-
+     
+# First phase of the app will ask user for text to predict an show the histroy of previous data inputs
 @app.route('/')
 def home():
     data = read_data_from_mongodb()
     return render_template('index.html', data=data)
 
-
+# Second phase of the app will show the prediction results
 @app.route('/predict', methods=['POST'])
 def predictor():
     if request.method == 'POST':
-        # User enter text
-
+        # if request is a json object, it will return a json object for the results 
         if request.is_json:
+            # text is a json object from the notebook
             sentence = request.get_json()['sentence']
+
+            # preprocess text: tokenize, stem, remove emojis, and stopwords
             processed_text = text_preprocessing_pipeline(sentence)
             processed_text_str = ' '.join(processed_text)
             
-            # vectorizer text
+            # vectorize text with the presaved vectorizer model
             vectorized_text = vectorizer.transform([processed_text_str])
 
-            # predict text using model
+            # predict text using XGBoost model
             text_prediction = model.predict(vectorized_text)
             text_prediction = int(text_prediction)
 
@@ -90,17 +83,19 @@ def predictor():
             prediction = {'prediction': pred_text}
             return json.dumps(prediction)
         
+        # else if the input is a string from the html form
         else: 
+            # user enter text
             user_text_input = request.form['text']
 
-            # preprocess text: tokenize, stemm, remove emojis, and stopwords
+            # preprocess text: tokenize, stem, remove emojis, and stopwords
             processed_text = text_preprocessing_pipeline(user_text_input)
             processed_text_str = ' '.join(processed_text)
 
-            # vectorizer text
+            # vectorize text with the presaved vectorizer model
             vectorized_text = vectorizer.transform([processed_text_str])
 
-            # predict text using model
+            # predict text using XGBoost model
             text_prediction = model.predict(vectorized_text)
             text_prediction = int(text_prediction)
 
@@ -116,12 +111,13 @@ def predictor():
             top_percents = [probs[label] for label in top_probs]
             top_preds = list(zip(top_labels, top_percents))
 
+            # get the date and time of predicting this text to add to MongoDB
             date_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
+            # save the input text, prediction result, and date-time in MongoDB
             client = MongoClient(MONGODB_URI)
             db = client.get_database(DB_NAME)
             collection = db[COLLECTION_NAME]
-
             data = {
                 'input': user_text_input,
                 'prediction': pred_text,
@@ -129,6 +125,7 @@ def predictor():
             }
             collection.insert_one(data)
 
+            # Update the displayed data
             updated_data = read_data_from_mongodb()
             client.close()
 
@@ -138,5 +135,5 @@ def predictor():
 
 if __name__ == "__main__":
     # app.run(host='127.0.0.1', port=8080, debug=True)
-    app.run(host='0.0.0.0', port=8080) # to access from outside of docker 
+    app.run(host='0.0.0.0', port=8080, debug=True) # to access from outside of docker 
 
